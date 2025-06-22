360170b29ba1b670235d7b1d5e66ee34deeb138e
LNKcode <3-tv-n@mail.ru>
2025-06-22 23:31:30 +0300

    добавил кнопке состояния при различных событиях

--- source/js/script.js
+++ source/js/script.js
@@ -2,7 +2,15 @@ const btnsImoprtant = document.querySelectorAll('.main-scope__btn');
 
 btnsImoprtant.forEach((item) => {
   item.addEventListener('click', () => {
-    item.classList.toggle('important');
+    item.closest('.main-scope__item').classList.toggle('important');
+  })
+})
+
+const descItems = document.querySelectorAll('.main-scope__desc');
+
+descItems.forEach((item) => {
+  item.addEventListener('click', () => {
+    item.classList.toggle('done');
   })
 })
 
@@ -11,16 +19,31 @@ const delBtnNode = document.querySelector('.main-scope__del-btn');
 const spanCounterNode = document.querySelector('.main-scope__del-btn span');
 
 
+
 checkboxes.forEach((item) => {
   item.addEventListener('change', () => {
+    let importantTask = false;
     if (item.checked) {
-      item.closest('.main-scope__item').classList.add('checked-item')
+      item.closest('.main-scope__item').classList.add('checked-item');
+      item.closest('.main-scope__item').querySelector('.main-scope__btn').setAttribute('disabled', true);
+
     } else {
-      item.closest('.main-scope__item').classList.remove('checked-item')
+      item.closest('.main-scope__item').classList.remove('checked-item');
+      item.closest('.main-scope__item').querySelector('.main-scope__btn').removeAttribute('disabled');
     }
-    let checkedItems = document.querySelectorAll('.checked-item').length;
-    spanCounterNode.textContent = `${checkedItems}`;
-    if (checkedItems === 0) {
+    let checkedItems = document.querySelectorAll('.checked-item');
+    spanCounterNode.textContent = `${checkedItems.length}`;
+    checkedItems.forEach((item) => {
+      if (item.classList.contains('important')) {
+        importantTask = true;
+      }
+    })
+    if (importantTask) {
+      delBtnNode.setAttribute('disabled', true);
+    } else {
+      delBtnNode.removeAttribute('disabled');
+    }
+    if (checkedItems.length === 0) {
       delBtnNode.classList.add('hidden')
     } else {
       if (delBtnNode.classList.contains('hidden')) {
@@ -29,3 +52,11 @@ checkboxes.forEach((item) => {
     }
   })
 })
+
+delBtnNode.addEventListener('click', () => {
+  document.querySelectorAll('.checked-item').forEach((item) => {
+    item.remove();
+  })
+  delBtnNode.classList.add('hidden')
+})
+
